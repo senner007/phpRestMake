960eb512a5e08f70ba97805e519674d35c691ddc
senner007 <nielshtg@hotmail.com>
2018-06-23 16:29:35 +0200

    set comments

--- config/login.php
+++ config/login.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-   header("Access-Control-Allow-Origin: http://localhost:3000");
-   header("Access-Control-Allow-Headers: *");
+   header("Access-Control-Allow-Origin: http://localhost:3000"); // should be set to host address on deploy
+   header("Access-Control-Allow-Headers: *"); // TODO : Fix -  https://stackoverflow.com/questions/8719276/cors-with-php-headers
    header('Access-Control-Allow-Credentials: true');
    // header('Content-Type: application/json');
 
@@ -17,7 +17,7 @@
 
             $_SESSION['myname'] = $_POST['myname'];
             echo "session set!";
-            return;
+            return; 
            //header('Location: ..\index.php');
           // header('Location: http://localhost:3000');
        }
